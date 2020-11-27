96f7e94bb8c41d12cc3119563d685393cf6dce98
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-27 16:05:07 -0300

    Implementada funcao fetch_content no scrapper.py

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,21 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        # recurso demora muito a responder
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.ReadTimeout:
+        # vamos fazer uma nova requisição
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    finally:
+        if response.status_code != 200:
+            return ""
+        else:
+            return response.text
 
 
 def scrape(fetcher, pages=1):
