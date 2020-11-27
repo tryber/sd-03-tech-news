1463e2cb902d8e6b3d10ec415c4392d88c96e6a1
Gustaff <gustavohmfigueiredo@gmail.com>
2020-11-27 19:11:28 -0300

    importer passing

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,36 @@
+import csv
+
+EXP_HEADERS = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath, "r") as file:
+            data = csv.DictReader(file, delimiter=";")
+            if data.fieldnames != EXP_HEADERS:
+                raise ValueError("Headers invalidos")
+            info = [
+                {key: value for key, value in line.items()} for line in data
+            ]
+
+    except FileNotFoundError:
+        print(f"############################# {filepath}")
+        filename = filepath.split("/")[-1]
+        raise ValueError(f"Arquivo {filename} não encontrado")
+    else:
+        return info
+
+
+csv_importer("correct.csv")
--- tech_news/database.py
+++ tech_news/database.py
@@ -21,11 +21,16 @@ def create_news(data):
 
 
 def insert_or_update(notice):
-    return  db.news.update_one({"url": notice['url']}, {"$set": notice}, upsert=True).upserted_id is not None
+    return (
+        db.news.update_one(
+            {"url": notice["url"]}, {"$set": notice}, upsert=True
+        ).upserted_id
+        is not None
+    )
 
 
 def find_news():
-    return list(db.news.find({}, {'_id': False}))
+    return list(db.news.find({}, {"_id": False}))
 
 
 def search_news(query):
