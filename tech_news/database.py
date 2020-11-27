e28643b8c75fc00aa9fd47b1bb12d44353f5fd89
Cardoso <lsc.cardosoo@gmail.com>
2020-11-27 16:35:00 -0300

    Flake8

--- tech_news/database.py
+++ tech_news/database.py
@@ -8,12 +8,12 @@ client = MongoClient(host=DB_HOST, port=int(DB_PORT))
 db = client.tech_news
 
 
-def insert_or_update(notice):
-    """Seu código deve vir aqui"""
+# def insert_or_update(notice):
+#     return ""
 
 
-def check_duplicates(news):
-    """Seu código deve vir aqui"""
+# def check_duplicates(news):
+#     return ""
 
 
 def create_news(data):
@@ -21,7 +21,9 @@ def create_news(data):
 
 
 def insert_or_update(notice):
-    return  db.news.update_one({"url": notice['url']}, {"$set": notice}, upsert=True).upserted_id is not None
+    return db.news.update_one(
+        {"url": notice['url']}, {"$set": notice}, upsert=True
+    ).upserted_id is not None
 
 
 def find_news():
