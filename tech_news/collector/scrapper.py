b7411b027cd775d08b809dc7e004d5dc7f201870
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-11-27 19:29:02 -0300

    Fetch_content_response.text

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,15 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout).text
+        time.sleep(delay)
+    except requests.HTTPError:
+        return ""
+    else:
+        return response
 
 
 def scrape(fetcher, pages=1):
