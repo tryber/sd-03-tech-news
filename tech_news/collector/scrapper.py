6627cc0732c663c06827909f4f79ba35ce838de8
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-27 19:10:41 -0300

    funcao scrape 50% pronta, pegando os dados de um artigo individualmente

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,7 @@
+from parsel import Selector
 import requests
 import time
+import re
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -18,5 +20,57 @@ def fetch_content(url, timeout=3, delay=0.5):
             return response.text
 
 
+def get_only_numbers(str):
+    result = ""
+    for character in str:
+        if re.search("[0-9]", character):
+            result = result + character
+    return result
+
+
+def no_white_spaces_around(str):
+    result = str
+    last_character = len(str)
+    if result[0] == " ":
+        result = result[1: last_character]
+        last_character = len(result)
+    if result[last_character - 1] == " ":
+        result = result[0: last_character - 1]
+    return result
+
+
+def no_list_item_white_spaces(list):
+    result = []
+    for items in list:
+        result.append(no_white_spaces_around(items))
+    return result
+
+
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    source = "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
+    response = fetcher(source)
+    selector = Selector(text=response)
+    url = "Link da url"
+    title = selector.css(".tec--article__header__title::text").get()
+    time = selector.css(".tec--timestamp__item time::attr(datetime)").get()
+    writer_text = selector.css(".tec--author__info__link::text").get()
+    writer = no_white_spaces_around(writer_text)
+    shares_text = selector.css(".tec--toolbar__item::text").get()
+    shares_count = int(get_only_numbers(shares_text))
+    comments = int(selector.css("#js-comments-btn::attr(data-count)").get())
+    summary = selector.css(".tec--article__body p *::text").get()
+    source_list = selector.css(".z--mb-16 .tec--badge::text").getall()
+    sources = no_list_item_white_spaces(source_list)
+    category_list = selector.css("#js-categories a::text").getall()
+    categories = no_list_item_white_spaces(category_list)
+    return [{
+        "url": url,
+        "title": title,
+        "timestamp": time,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+    }]
