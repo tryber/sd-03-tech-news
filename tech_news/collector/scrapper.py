f7f86e03b37f13c383d3a4a40c11d168520ba206
Cardoso <lsc.cardosoo@gmail.com>
2020-11-27 16:29:29 -0300

    Criar funçao que responde a requisiçao

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.HTTPError:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    return ""
+
+# print("Ola mundo")
