7d334c6e4a22eee089cf0f62998b69fca129471b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-11-27 23:13:26 -0300

    Funcao scrape para pagina unica 100% funcional

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -46,6 +46,13 @@ def no_list_item_white_spaces(list):
     return result
 
 
+# Ref.: https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
 def scrape(fetcher, pages=1):
     source = "https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"
     response = fetcher(source)
@@ -58,8 +65,8 @@ def scrape(fetcher, pages=1):
     shares_text = selector.css(".tec--toolbar__item::text").get()
     shares_count = int(get_only_numbers(shares_text))
     comments = int(selector.css("#js-comments-btn::attr(data-count)").get())
-    summary = selector.css(".tec--article__body p").get()
-    # summary2 = selector.css(f"{summary} *::text").get()
+    summary_html = selector.css(".tec--article__body p").get()
+    summary = cleanhtml(summary_html)
     source_list = selector.css(".z--mb-16 .tec--badge::text").getall()
     sources = no_list_item_white_spaces(source_list)
     category_list = selector.css("#js-categories a::text").getall()
