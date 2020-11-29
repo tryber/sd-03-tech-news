4c436f6ff239c486e27741abdc83d0c573d167dc
willy <wscatao@gmail.com>
2020-11-29 09:45:01 -0300

    Requisito 1 feito

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,17 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    strvar = ""
+    # Faz uma request com um timeout de até 3 segundos e só refaz após o delay
+    response = requests.get(url, timeout=timeout)
+    # Analisa se houve uma resposta do server.
+    if response.status_code != 200:
+        return strvar
+    else:
+        return response.text
+    time.sleep(delay)
 
 
 def scrape(fetcher, pages=1):
