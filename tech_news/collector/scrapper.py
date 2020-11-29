e5b01784450f2bf33d297f87b00df09da7b30fcf
DiegoRafaelAF <draf0314@gmail.com>
2020-11-29 18:57:33 -0300

    Dois testes passando localmente

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
+BASE_URL = "https://www.tecmundo.com.br/"
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    time.sleep(delay)
+    try:
+        response = requests.get(url, timeout=timeout)
+    except (response.HTTPError, response.ReadTimeout):
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+
