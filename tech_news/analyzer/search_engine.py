aa90ab0771e209aa8787abd69ef66929cde3f820
Gustaff <gustavohmfigueiredo@gmail.com>
2020-11-30 17:04:55 -0300

    search_engine ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
-from tech_news.database import search_news, get_collection
 import re
+from datetime import datetime
+from tech_news.database import get_collection
 
 
 def search_by_title(title):
@@ -11,15 +12,28 @@ def search_by_title(title):
 
 
 def search_by_date(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
     results = get_collection().find(
-        {"date": date}, {"title": True, "_id": False, "url": True}
+        {"timestamp": {"$regex": re.compile(date)}},
+        {"title": True, "_id": False, "url": True},
     )
     return [(result["title"], result["url"]) for result in results]
 
 
 def search_by_source(source):
-    return search_news({"source": {"$match": source}})
+    results = get_collection().find(
+        {"sources": {"$regex": re.compile(source, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
 
 
 def search_by_category(category):
-    return search_news({"category": {"$match": category}})
+    results = get_collection().find(
+        {"categories": {"$regex": re.compile(category, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
