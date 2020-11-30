f1a879c8251415b150be313875175379102d518e
fabemiliano <fab.emiliano@gmail.com>
2020-11-30 18:21:55 -0300

    requiste 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -13,6 +13,29 @@ baseArr = [
 ]
 
 
+def appendArr(arr, result):
+    header, *data = result
+    header.sort()
+    if header != baseArr:
+        raise ValueError("Formato invalido aqui")
+    for row in data:
+        url, title, timestamp, writer, shares_count, *second_half = row
+        comments_count, summary, sources, categories = second_half
+        arr.append(
+            {
+                "url": url,
+                "title": title,
+                "timestamp": timestamp,
+                "writer": writer,
+                "shares_count": shares_count,
+                "comments_count": comments_count,
+                "summary": summary,
+                "sources": sources,
+                "categories": categories,
+            }
+        )
+
+
 def csv_importer(filepath):
     arr = []
     try:
@@ -20,26 +43,7 @@ def csv_importer(filepath):
             raise ValueError("Formato invalido")
         with open(filepath) as file:
             result = csv.reader(file, delimiter=";", quotechar='"')
-            header, *data = result
-            header.sort()
-            if header != baseArr:
-                raise ValueError("Formato invalido aqui")
-            for row in data:
-                url, title, timestamp, writer, shares_count, *second_half = row
-                comments_count, summary, sources, categories = second_half
-                arr.append(
-                    {
-                        "url": url,
-                        "title": title,
-                        "timestamp": timestamp,
-                        "writer": writer,
-                        "shares_count": shares_count,
-                        "comments_count": comments_count,
-                        "summary": summary,
-                        "sources": sources,
-                        "categories": categories,
-                    }
-                )
+            appendArr(arr, result)
     except FileNotFoundError:
         raise ValueError("Arquivo file_not_exist.csv não encontrado")
     else:
