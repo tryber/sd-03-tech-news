08d6ddbb7b9516305b6a9eb3e6308d58c82971c0
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-11-30 19:22:03 -0300

    fetcher-requisite 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,16 +1,53 @@
 import requests
 import time
+from parsel import Selector
+
+
+def sleep(delay):
+    time.sleep(delay)
 
 
 def fetch_content(url, timeout=3, delay=0.5):
+    sleep(delay)
     try:
-        response = requests.get(url, timeout=timeout).text
-        time.sleep(delay)
-    except requests.HTTPError:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except (requests.ReadTimeout, requests.HTTPError):
         return ""
     else:
-        return response
+        return response.text
+
+
+def get_url(fetcher):
+    selector = Selector(fetcher)
+    page = selector.css(
+        ".tec--list__item .tec--card__title__link::attr(href)")
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+
+    selector = Selector(fetcher)
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = selector.css(".tec--author__info__link::text").get()
+    shares_count = selector.css(".tec--toolbar__item::text").getall()
+    comments_count = selector.css("#js-comments-btn::text").getall()
+    summary = selector.css("div.tec--article__body > p::text").get()
+    sources = selector.css("div.z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+
+    lista = [{
+                "title": title,
+                "timestamp": timestamp,
+                "writer": writer,
+                "shares_count": shares_count,
+                "comments_count": comments_count,
+                "summary": summary,
+                "sources": sources,
+                "categories": categories,
+            }]
+
+    return lista
