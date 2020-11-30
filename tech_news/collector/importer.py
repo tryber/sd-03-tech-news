22c778aeefdfae1652110327460ce840d846980b
fabemiliano <fab.emiliano@gmail.com>
2020-11-30 13:52:53 -0300

    first requisite

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,4 +1,4 @@
-import csv
+# import csv
 
 baseArr = [
     "categories",
@@ -15,33 +15,36 @@ baseArr = [
 arr = []
 new_source = []
 new_category = []
-def csv_importer(filepath):
-    try:
-        if filepath[-4:] != ".csv":
-            raise ValueError("Formato invalido")
-        with open(filepath) as file:
-            result = csv.reader(file, delimiter=";", quotechar='"')
-            header, *data = result
-            header.sort()
-            if header != baseArr:
-                raise ValueError("Formato invalido aqui")
-            for row in data:
-                url, title, timestamp, writer, shares_count, comments_count, summary, sources, categories = row
-                arr.append({
-                    "url": url, 
-                    "title": title, 
-                    "timestamp": timestamp, 
-                    "writer": writer, 
-                    "shares_count": shares_count, 
-                    "comments_count": comments_count, 
-                    "summary": summary, 
-                    "sources": sources,
-                    "categories": categories
-                })  
-    except FileNotFoundError:
-        (f"Arquivo {filepath} não encontrado")
-    else:
-        return arr
 
 
-print(csv_importer('correct.csv'))
+# def csv_importer(filepath):
+#     try:
+#         if filepath[-4:] != ".csv":
+#             raise ValueError("Formato invalido")
+#         with open(filepath) as file:
+#             result = csv.reader(file, delimiter=";", quotechar='"')
+#             header, *data = result
+#             header.sort()
+#             if header != baseArr:
+#                 raise ValueError("Formato invalido aqui")
+#             for row in data:
+#                 url, title, timestamp, writer, shares_count, comments_count
+#  summary, sources, categories = row
+#                 arr.append({
+#                     "url": url,
+#                     "title": title,
+#                     "timestamp": timestamp,
+#                     "writer": writer,
+#                     "shares_count": shares_count,
+#                     "comments_count": comments_count,
+#                     "summary": summary,
+#                     "sources": sources,
+#                     "categories": categories
+#                 })
+#     except FileNotFoundError:
+#         (f"Arquivo {filepath} não encontrado")
+#     else:
+#         return arr
+
+
+# print(csv_importer('correct.csv'))
--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,10 +1,11 @@
 import requests
 import time
-from parsel import Selector
+# from parsel import Selector
 
 
 # def fetch_content(url, timeout=3, delay=0.5):
 #     """Seu código deve vir aqui"""
+
 
 def sleep(delay):
     time.sleep(delay)
@@ -15,65 +16,71 @@ def fetch_content(url, timeout=3, delay=0.5):
         response = requests.get(url, timeout=timeout)
         sleep(delay)
     except requests.ReadTimeout:
-        return ''
+        return ""
     else:
         if response.status_code != 200:
-            return ''
+            return ""
         else:
             return response.text
 
-arr = []
-
 
 def scrape(fetcher, pages=1):
-    for i in range(pages):
-        page = requests.get(
-            f"https://www.tecmundo.com.br/novidades?page={i + 1}"
-        )
-        selector = Selector(text=page.text)
-        links = selector.css(".tec--card__title__link::attr(href)").getall()
-        for link in links:
-            try:
-                second_response = requests.get(link)
-                infos = Selector(text=second_response.text)
-                title = infos.css(".tec--article__header__title::text").get()
-                timestamp = infos.css(
-                    ".tec--timestamp__item time::attr(datetime)"
-                ).get()
-                writer = infos.css(".tec--author__info__link::text").get()
-                shares_count = infos.css(".tec--toolbar__item::text").get()
-                suffix = " Compartilharam"
-                if shares_count.endswith(suffix):
-                    shares_count = shares_count[: -len(suffix)]
-                comments_count = infos.css(".tec--btn::attr(data-count)").get()
-                summary = infos.css(".tec--article__body *::text").get()
-                print(summary)
-                sources = []
-                for source in infos.css(
-                    ".z--mb-16 .tec--badge::text"
-                ).getall():
-                    sources.append(source)
-                categories = []
-                for category in infos.css(
-                    ".tec--badge--primary::text"
-                ).getall():
-                    categories.append(category)
-                arr.append(
-                    {
-                        "url": link,
-                        "title": title,
-                        "timestamp": timestamp,
-                        "writer": writer,
-                        "shares_count": shares_count,
-                        "comments_count": comments_count,
-                        "summary": summary,
-                        "sources": sources,
-                        "categories": categories,
-                    }
-                )
-            except AttributeError:
-                arr.append({'vazio': 0})
-    print(arr)
+    """Seu código deve vir aqui"""
+
+
+# arr = []
+
+
+# def scrape(fetcher, pages=1):
+#     for i in range(pages):
+#         page = requests.get(
+#             f"https://www.tecmundo.com.br/novidades?page={i + 1}"
+#         )
+#         selector = Selector(text=page.text)
+#         links = selector.css(".tec--card__title__link::attr(href)").getall()
+#         for link in links:
+#             try:
+#                 second_response = requests.get(link)
+#                 infos = Selector(text=second_response.text)
+#                 title = infos.css(".tec--article__header__title::text").get()
+#                 timestamp = infos.css(
+#                     ".tec--timestamp__item time::attr(datetime)"
+#                 ).get()
+#                 writer = infos.css(".tec--author__info__link::text").get()
+#                 shares_count = infos.css(".tec--toolbar__item::text").get()
+#                 suffix = " Compartilharam"
+#                 if shares_count.endswith(suffix):
+#                     shares_count = shares_count[: -len(suffix)]
+#                 comments_count = infos.css(".tec--btn::attr(data-count)")
+# .get()
+#                 summary = infos.css(".tec--article__body *::text").get()
+#                 print(summary)
+#                 sources = []
+#                 for source in infos.css(
+#                     ".z--mb-16 .tec--badge::text"
+#                 ).getall():
+#                     sources.append(source)
+#                 categories = []
+#                 for category in infos.css(
+#                     ".tec--badge--primary::text"
+#                 ).getall():
+#                     categories.append(category)
+#                 arr.append(
+#                     {
+#                         "url": link,
+#                         "title": title,
+#                         "timestamp": timestamp,
+#                         "writer": writer,
+#                         "shares_count": shares_count,
+#                         "comments_count": comments_count,
+#                         "summary": summary,
+#                         "sources": sources,
+#                         "categories": categories,
+#                     }
+#                 )
+#             except AttributeError:
+#                 arr.append({"vazio": 0})
+#     print(arr)
 
 
 # scrape("https://www.tecmundo.com.br/novidades")
@@ -91,7 +98,8 @@ def scrape(fetcher, pages=1):
 #         second_response = requests.get(link)
 #         infos = Selector(text=second_response.text)
 #         header = infos.css(".tec--article__header__title::text").get()
-#         timestamp = infos.css(".tec--timestamp__item time").attrib['datetime']
+#         timestamp = infos.css(".tec--timestamp__item time").attrib['datetime'
+# ]
 #         writer = infos.css(".tec--author__info__link::text").get()
 #         shares_count = infos.css(".tec--toolbar__item::text").get()
 #         comments_count = infos.css(".tec--btn::text").get()
