0a85c0a0c0c52319d5d5bfdd2015c966d8df3ae8
luis.campos <gucampos845@gmail.com>
2020-11-30 15:57:54 -0300

    Primeiro commit

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+    except:
+        return ""
 
+    if response.status_code == 200:
+        return response.text
+    else:
+        return ""
+    time.sleep(delay)
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
