4ce54b7be477127b8393c93017489651e07b23e6
luizfernando <luizfernandotic@gmail.com>
2020-12-01 17:16:28 -0300

    requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,15 @@
+# from parsel import Selector
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
-
-
-def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+        response.raise_for_status()
+    except (requests.ReadTimeout, requests.HTTPError) as excep:
+        print(excep)
+        return ''
+    else:
+        return response.text
