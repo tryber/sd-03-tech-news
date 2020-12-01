5bd517024a2990750625c57d3117e2aeebf7ed7b
Rhian Castro <rhi.castro@gmail.com>
2020-12-01 14:02:19 -0300

    lint

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,19 +1,17 @@
 from time import sleep
 import requests
-import time
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     try:
         response = requests.get(url, timeout=timeout)
-        time.sleep(delay)
+        sleep(delay)
     except requests.ReadTimeout:
         return ""
     finally:
         if response.status_code != 200:
             return ""
         return response.text
-
 
 
 def scrape(fetcher, pages=1):
