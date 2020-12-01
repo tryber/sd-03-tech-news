a31404267cdb84119b3eb245db3e63c36dea8d4a
luldelrey <rpgstrikered@gmail.com>
2020-12-01 19:17:56 -0300

    saving progress

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,15 @@
+import requests
+
+
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+    res = requests.get(url)
+    print(res.text)
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    fetch_content('https://www.tecmundo.com.br/novidades')
