7107968683bb63dd8fea5c58e070abc8b589fd3a
Rhian Castro <rhi.castro@gmail.com>
2020-12-01 13:58:31 -0300

    fetch_content

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,19 @@
+from time import sleep
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+    except requests.ReadTimeout:
+        return ""
+    finally:
+        if response.status_code != 200:
+            return ""
+        return response.text
+
 
 
 def scrape(fetcher, pages=1):
