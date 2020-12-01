a8a409898203cf54c2663a4b2980b05f52b30982
fabemiliano <fab.emiliano@gmail.com>
2020-11-30 21:54:05 -0300

    requiste 8 + decouple

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
+from decouple import config
 import csv
 
-client = MongoClient()
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+client = MongoClient(host=DB_HOST, port=int(DB_PORT))
 db = client.tech_news
 
 
