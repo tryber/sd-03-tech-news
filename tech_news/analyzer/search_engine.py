46e2e3371802dab8b095821138b92c63a239452d
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-12-02 18:42:00 -0300

    search engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,39 @@
+import re
+from datetime import datetime
+from tech_news.database import get_collection
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    results = get_collection().find(
+        {"title": {"$regex": re.compile(title, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    results = get_collection().find(
+        {"timestamp": {"$regex": re.compile(date)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    results = get_collection().find(
+        {"sources": {"$regex": re.compile(source, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    results = get_collection().find(
+        {"categories": {"$regex": re.compile(category, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True},
+    )
+    return [(result["title"], result["url"]) for result in results]
