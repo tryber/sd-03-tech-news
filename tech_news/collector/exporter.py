3421e7209d98668f80d5e398ec6fe7de9eb67563
William Igor <williamigorsilva@gmail.com>
2020-12-04 16:34:32 -0300

    exporter csv

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,5 +1,41 @@
 import csv
+from tech_news.database import find_news
+
+
+HEADERS_EXPECTED = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories",
+]
+
+
+def validFile(file):
+    if not file.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    return file.split("/")[-1]
 
 
 def csv_exporter(filepath):
-    ""
+    validFile(filepath)    
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(HEADERS_EXPECTED)
+        for news in find_news():
+            var = [
+                news["url"],
+                news["title"],
+                news["timestamp"],
+                news["writer"],
+                str(news["shares_count"]),
+                str(news["comments_count"]),
+                news["summary"],
+                ",".join(news["sources"]),
+                ",".join(news["categories"]),
+            ]
+            writer.writerow(var)
