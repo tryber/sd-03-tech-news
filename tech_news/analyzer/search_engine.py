9a121e3223d44d2cb45afc5131b48009352004e5
luis.campos <gucampos845@gmail.com>
2020-12-05 16:10:56 -0300

    Requisito 6, 7, 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from tech_news.database import db
 
 
@@ -15,12 +16,40 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    news_list = db.news.find(
+        {"timestamp": {"$regex": re.compile(date)}},
+        {"title": True, "_id": False, "url": True}
+    )
+
+    for new in news_list:
+        return [(new["title"], new["url"])]
+
+    return []
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_list = db.news.find(
+        {"sources": {"$regex": re.compile(source, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True}
+    )
+
+    for new in news_list:
+        return [(new["title"], new["url"])]
+
+    return []
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_list = db.news.find(
+        {"categories": {"$regex": re.compile(category, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True}
+    )
+
+    for new in news_list:
+        return [(new["title"], new["url"])]
+
+    return []
