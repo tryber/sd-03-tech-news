082e5a2f5c278dd40020e088d3627f653974943b
luis.campos <gucampos845@gmail.com>
2020-12-05 15:46:30 -0300

    Requisito 5

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,15 +1,17 @@
+import re
 from tech_news.database import db
 
 
 def search_by_title(title):
-    new_list = []
-    try:
-        for new in db.news.find({"title": {"$regex": title}}):
-            new_list.append(new)
-    except not new_list:
-        return []
-    else:
-        return new_list
+    news_list = db.news.find(
+        {"title": {"$regex": re.compile(title, re.IGNORECASE)}},
+        {"title": True, "_id": False, "url": True}
+    )
+
+    for new in news_list:
+        return [(new["title"], new["url"])]
+
+    return []
 
 
 def search_by_date(date):
