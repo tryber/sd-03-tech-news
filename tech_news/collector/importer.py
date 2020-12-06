3fbf367bbf3197a85552220e917ae4ab9c1b9ead
Marco Barbosa <marco.meireles.b@gmail.com>
2020-12-05 23:55:28 -0300

    Refatorando requisito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -14,25 +14,28 @@ HEADER = [
 ]
 
 
+def check_path(filepath):
+    if not Path(filepath).suffix == ".csv":
+        raise ValueError("Formato invalido")
+    return filepath.split("/")[-1]
+
+
 def csv_importer(filepath):
     news_summary = []
-    if not Path(filepath).suffix == ".csv":
-        raise ValueError("Extensão inválida")
-    with open(filepath) as file:
-        news = csv.reader(file, delimiter=";", quotechar='"')
-        header, *data = news
-        if header != HEADER:
-            raise ValueError("Conteúdo inválido")
-        for news in data:
-            news_summary.append({
-                header[0]: news[0],
-                header[1]: news[1],
-                header[2]: news[2],
-                header[3]: news[3],
-                header[4]: int(news[4]),
-                header[5]: int(news[5]),
-                header[6]: news[6],
-                header[7]: news[7],
-                header[8]: news[8]
+    file = check_path(filepath)
+    try:
+        with open(filepath) as file:
+            news = csv.reader(file, delimiter=";", quotechar='"')
+            header, *data = news
+            if header != HEADER:
+                raise ValueError("Headers invalidos")
+            # ref.:
+            # https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+            for news in data:
+                news_summary.append({
+                    HEADER[i]: news[i] for i in range(len(HEADER))
                 })
-    return news_summary
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {file} não encontrado")
+    else:
+        return news_summary
