a2f6afdc2d0525d0284405a11f825a8a548c958c
Marco Barbosa <marco.meireles.b@gmail.com>
2020-12-05 23:07:44 -0300

    Requisitos 3 e 4 passando nos testes manuais

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,36 @@
+from pathlib import Path
+import csv
+from tech_news.database import find_news
+
+HEADER = [
+    'url',
+    'title',
+    'timestamp',
+    'writer',
+    'shares_count',
+    'comments_count',
+    'summary',
+    'sources',
+    'categories'
+]
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not Path(filepath).suffix == ".csv":
+        raise ValueError("Extensão inválida")
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(HEADER)
+        for news in find_news():
+            output = [
+                news["url"],
+                news["title"],
+                news["timestamp"],
+                news["writer"],
+                str(news["shares_count"]),
+                str(news["comments_count"]),
+                news["summary"],
+                ",".join(news["sources"]),
+                ",".join(news["categories"]),
+            ]
+            writer.writerow(output)
--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,38 @@
+from pathlib import Path
+import csv
+
+HEADER = [
+    'url',
+    'title',
+    'timestamp',
+    'writer',
+    'shares_count',
+    'comments_count',
+    'summary',
+    'sources',
+    'categories'
+]
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    news_summary = []
+    if not Path(filepath).suffix == ".csv":
+        raise ValueError("Extensão inválida")
+    with open(filepath) as file:
+        news = csv.reader(file, delimiter=";", quotechar='"')
+        header, *data = news
+        if header != HEADER:
+            raise ValueError("Conteúdo inválido")
+        for news in data:
+            news_summary.append({
+                header[0]: news[0],
+                header[1]: news[1],
+                header[2]: news[2],
+                header[3]: news[3],
+                header[4]: int(news[4]),
+                header[5]: int(news[5]),
+                header[6]: news[6],
+                header[7]: news[7],
+                header[8]: news[8]
+                })
+    return news_summary
