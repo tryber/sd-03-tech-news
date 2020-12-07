be669721193a9359812e5d3d30edc4e1f4655508
Cardoso <lsc.cardosoo@gmail.com>
2020-12-06 22:57:24 -0300

    Requisitos 5,6,7,8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,56 @@
+from tech_news.database import search_news
+from datetime import datetime
+import re
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    notices = search_news({
+        "title": {'$regex':  f"^{ title }$", "$options": '-i'}
+    })
+    data = []
+    for notice in notices:
+        data.append((
+            notice['title'],
+            notice['url'],
+        ))
+    return data
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+    notices = search_news({"timestamp": {"$regex": date}})
+    data = []
+    for notice in notices:
+        data.append((
+            notice['title'],
+            notice['url'],
+        ))
+    return data
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    notices = search_news({
+        "sources": {"$all": [re.compile(source, re.IGNORECASE)]}
+    })
+    data = []
+    for notice in notices:
+        data.append((
+            notice['title'],
+            notice['url'],
+        ))
+    return data
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    notices = search_news({
+        "categories": {"$all": [re.compile(category, re.IGNORECASE)]}
+    })
+    data = []
+    for notice in notices:
+        data.append((
+            notice['title'],
+            notice['url'],
+        ))
+    return data
