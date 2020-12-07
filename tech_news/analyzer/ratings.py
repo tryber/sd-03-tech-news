46d113412bf954b6ed8446267b9d0424560f8961
Marco Barbosa <marco.meireles.b@gmail.com>
2020-12-07 09:42:18 -0300

    Requisito 9 ok

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,27 @@
+from tech_news.database import aggregate_filter
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_five = []
+    for element in aggregate_filter(
+        [
+            {
+                "$addFields": {
+                    "total": {"$add": ["$shares_count", "$comments_count"]}
+                },
+            },
+            {
+                "$sort": {"total": -1, "title": 1},
+            },
+            {
+                "$limit": 5,
+            },
+        ]
+    ):
+        title = element["title"]
+        url = element["url"]
+        top_five.append((title, url))
+    return top_five
 
 
 def top_5_categories():
--- tech_news/database.py
+++ tech_news/database.py
@@ -27,3 +27,7 @@ def find_news():
 
 def search_news(query):
     return list(db.news.find(query))
+
+
+def aggregate_filter(query):
+    return db.news.aggregate(query)
