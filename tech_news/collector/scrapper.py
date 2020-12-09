978fef39e9e642b2f7d942c46c4dd071768fa8cf
luldelrey <rpgstrikered@gmail.com>
2020-12-08 22:50:56 -0300

    saving progress but fetch content already implemented

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,15 +1,26 @@
+from time import sleep
+from parsel import Selector
 import requests
 
 
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
-    res = requests.get(url)
-    print(res.text)
+    sleep(delay)
+    res = requests.get(url, timeout=timeout)
+    if res.status_code != 200:
+        return ""
+    return res.text
 
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
+    url = f"https://www.tecmundo.com.br/novidades?page={pages}"
+    response = fetch_content(url)
+    selector = Selector(text=response)
+    news = selector.css(".tec--card__title__link::attr(href)").getall()
+    return news
 
 
 if __name__ == "__main__":
-    fetch_content('https://www.tecmundo.com.br/novidades')
+    # print(fetch_content('https://www.tecmundo.com.br/novidades'))
+    print(scrape(fetch_content, 1))
