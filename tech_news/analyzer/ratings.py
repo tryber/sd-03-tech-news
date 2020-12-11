daba7cf78f61e2854333f082ffb7f83c75ecf957
rdgbrezolin <brezolinr@gmail.com>
2020-12-11 11:08:23 -0300

    local100

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,31 @@
+from tech_news.database import get_collection
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_5 = get_collection().aggregate(
+        [
+            {
+                "$addFields": {
+                    "total_shares_and_comments": {
+                        "$sum": ["$shares_count", "$comments_count"]
+                    }
+                },
+            },
+            {"$sort": {"total_shares_and_comments": -1, "title": 1}},
+            {"$limit": 5},
+            {"$project": {"_id": False, "title": True, "url": True}},
+        ]
+    )
+    return [(category["title"], category["url"]) for category in top_5]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top_5 = get_collection().aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "quantity": {"$sum": 1}}},
+            {"$sort": {"quantity": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    return [category["_id"] for category in top_5]
--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,24 @@
+import csv
+from tech_news.database import find_news
+from tech_news.collector.csv_helpers import validate_filepath, EXP_HEADERS
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    validate_filepath(filepath)
+
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(EXP_HEADERS)
+        for document in find_news():
+            dict = [
+                document["url"],
+                document["title"],
+                document["timestamp"],
+                document["writer"],
+                str(document["shares_count"]),
+                str(document["comments_count"]),
+                document["summary"],
+                ",".join(document["sources"]),
+                ",".join(document["categories"]),
+            ]
+            writer.writerow(dict)
