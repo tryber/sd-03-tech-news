2a74c93c7358bde3262530a3125f2c1f5352c120
clayton pereira <cpereiramt@gmail.com>
2020-12-12 15:18:57 -0400

    salvando ultimas alteraçoes requisito 01

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,4 +1,5 @@
 import requests
+from parsel import Selector
 from time import sleep
 
 
@@ -18,4 +19,36 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    data = []
+    for info in range(int(pages)):
+        selector = Selector(
+            text=fetcher("https://www.tecmundo.com.br/novidades")
+        )
+        mainData = selector.css(
+            ".tec--list .tec--card__title__link::attr(href)"
+        ).getall()
+        for url in mainData:
+            selector = Selector(text=fetcher(url))
+            data.append({
+                "url": url,
+                "title": selector.css(
+                    ".tec--article__header__title::text"
+                ).get(),
+                "timestamp": selector.css(
+                    "#js-article-date::attr(datetime)"
+                ).get(),
+                "writer": selector.css(".tec--author__info__link::text").get(),
+                "shares_count": int(selector.css(
+                    ".tec--toolbar__item::text"
+                ).re_first(r"\d+")),
+                "comments_count": int(selector.css(
+                    "#js-comments-btn::attr(data-count)"
+                ).get()),
+                "summary": selector.css(".tec--article__body > p::text").get(),
+                "sources": selector.css(".z--mb-16 div a::text").getall(),
+                "categories": selector.css(
+                    ".tec--badge.tec--badge--primary::text"
+                ).getall()
+            })
+    return data
+ 
