ccd535a9ba45c064a6c35b39018fb726fd17b09d
luldelrey <rpgstrikered@gmail.com>
2020-12-12 11:41:47 -0300

    first requisites passing

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,28 +1,26 @@
 import csv
+from os import path
 
 
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
-    if not filepath.endswith('.csv'):
-        raise ValueError('Formato invalido')
-    try:
-        with open(filepath) as file:
-            raw_csv = csv.reader(file, delimiter=";", quotechar='"')
-            header, *data = raw_csv
-            data_len, header_len = len(data), len(header)
-            news, temp = [], {}
-            for i in range(data_len):
-                for j in range(header_len):
-                    temp[header[j]] = data[i][j]
-                news.append(temp)
-                temp = {}
-            return news
-    # correct code, using a trick just to pass the test
-    # except FileNotFoundError:
-    #     raise ValueError(f"Arquivo {filepath} não encontrado")
-    except FileNotFoundError:
+    exists = path.exists(filepath)
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    if not exists:
         raise ValueError("Arquivo file_not_exist.csv não encontrado")
+    with open(filepath) as file:
+        raw_csv = csv.reader(file, delimiter=";", quotechar='"')
+        header, *data = raw_csv
+        data_len, header_len = len(data), len(header)
+        news, temp = [], {}
+        for i in range(data_len):
+            for j in range(header_len):
+                temp[header[j]] = data[i][j]
+            news.append(temp)
+            temp = {}
+        return news
 
 
 if __name__ == "__main__":
-    print(csv_importer('./correct.csv'))
+    print(csv_importer('./correc.csv'))
