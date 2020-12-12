1dc5adfe49df1aebdc2e6773363c15a2d4731396
luldelrey <rpgstrikered@gmail.com>
2020-12-12 18:20:31 -0300

    exporter done

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,10 +1,33 @@
 import csv
-from os import path
+from pymongo import MongoClient
 
 
 def csv_exporter(filepath):
     """Seu código deve vir aqui"""
     if not filepath.endswith(".csv"):
         raise ValueError("Formato invalido")
-    with open(filepath) as file:
-        csv.writer(delimiter=";", quotechar='"')
+    client = MongoClient()
+    db = client.tech_news
+    news = db.news.find()
+    header = list(news[0].keys())[1:]
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(header)
+        rows = []
+        for new in news:
+            temp = []
+            for elem in list(new.values())[1:]:
+                if isinstance(elem, list):
+                    espacer = ','
+                    new_elem = espacer.join(elem)
+                    temp.append(new_elem)
+                else:
+                    temp.append(elem)
+            rows.append(temp)
+            temp = []
+        writer.writerows(rows)
+    client.close()
+
+
+if __name__ == "__main__":
+    csv_exporter("a.csv")
