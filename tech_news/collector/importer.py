b0110f8900e246981994778bac7f10a5b129ea90
clayton pereira <cpereiramt@gmail.com>
2020-12-12 18:14:00 -0400

    Salvando alteraçoes no requisito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -2,12 +2,21 @@ import csv
 
 
 def csv_importer(filepath):
-    result = filepath.endswith("csv")
-    print(result)
-    if not result:
-        raise ValueError("Formato invalido")
-    with open(filepath) as file:
-        beach_status_reader = csv.reader(file, delimiter=",", quotechar='"')
-        header, *data = beach_status_reader
+    csv_data = []
+    try:
+        result = filepath.endswith("csv")
 
-        return header, data
+        if not result:
+            raise ValueError("Formato invalido")
+        with open(filepath) as file:
+            beach_status_reader = csv.reader(file,
+                                             delimiter=";", quotechar='"')
+            header, *data = beach_status_reader
+            dict = {}
+            for row, index in enumerate(header):
+                dict[index] = data[0][row]
+        csv_data.append(dict)
+        print(csv_data[0]["timestamp"])
+        return csv_data
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
