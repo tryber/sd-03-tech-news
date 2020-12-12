67cfeeabf06e3967d33b867fbe6dd14f601e8aa5
clayton pereira <cpereiramt@gmail.com>
2020-12-12 19:50:43 -0400

    Salvando alteraçoes no requisito 4

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,47 @@
+from tech_news.database import find_news
+import csv
+
+
+header = [
+    "url",
+    "title",
+    "timestamp",
+    "writer",
+    "shares_count",
+    "comments_count",
+    "summary",
+    "sources",
+    "categories"
+    ]
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith("csv"):
+        raise ValueError('Formato invalido')
+    data_from_database = find_news()
+    with open('export_correct.csv', mode='w') as data_file:
+        filewrite = csv.writer(data_file, delimiter=';')   
+        filewrite.writerow(header)
+        for file in data_from_database:
+            url = file["url"]
+            title = file["title"]
+            timestamp = file["timestamp"]
+            writer = file["writer"]
+            shares_count = file["shares_count"]
+            comments_count = file["comments_count"]
+            summary = file["summary"]
+            sources = "".join(file["sources"])
+            categories = ",".join(file["categories"])
+        filewrite.writerow(
+            [url,
+             title,
+             timestamp,
+             writer,
+             shares_count,
+             comments_count,
+             summary,
+             sources,
+             categories,
+             ]
+        )
+   
