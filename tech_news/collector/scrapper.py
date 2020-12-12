a21f4d5bf92b290ceef6a9c556da1dfe5986c448
clayton pereira <cpereiramt@gmail.com>
2020-12-12 13:54:22 -0400

    salvando alteraçoes no requisito 01

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,16 +1,20 @@
 import requests
+from time import sleep
 
 
 def fetch_content(url, timeout=3, delay=0.5):
-
-    # Requisição do tipo GET
-    response = requests.get(url)
-    if response.status_code != 200:
-        return ""
-    else:
-        return response.text
-    # print(response.headers["Content-Type"])  # conteúdo no formato html
-
+    try:
+        sleep(delay)
+        # Requisição do tipo GET
+        response = requests.get(url, timeout=timeout)
+        if response.status_code != 200:
+            return ""
+        else:
+            return response.text
+        # print(response.headers["Content-Type"])  # conteúdo no formato html
+    except (requests.ReadTimeout, requests.HTTPError) as excep:
+        print(excep)
+        return ''
 
 def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
