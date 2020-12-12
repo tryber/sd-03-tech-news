61db332ef27263d87ff2925f4970acaade04d73f
clayton pereira <cpereiramt@gmail.com>
2020-12-12 13:34:06 -0400

    salvando alteraçoes iniciais requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,15 @@
+import requests
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+
+    # Requisição do tipo GET
+    response = requests.get(url)
+    if response.status_code != 200:
+        return ""
+    else:
+        return response.text
+    # print(response.headers["Content-Type"])  # conteúdo no formato html
 
 
 def scrape(fetcher, pages=1):
