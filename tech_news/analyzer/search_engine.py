f75a49bbe40d30ffb126dfc60dcbc8e7bd7247f9
clayton pereira <cpereiramt@gmail.com>
2020-12-26 10:48:36 -0400

    salvando alteraçoes requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,42 @@
+import tech_news.database as db
+from datetime import datetime
+import re
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    result = db.search_news({"title": {
+                          "$regex": f"^{title}$", "$options": "-i"}
+                })
+    if result == []:
+        return result
+
+    return [(result[0]["title"], result[0]["url"])]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    result = db.search_news({"timestamp": {"$regex": date}})
+    if result == []:
+        return result
+    return [(result[0]["title"], result[0]["url"])]
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    result = db.search_news({"sources": {
+        "$all": [re.compile(source, re.IGNORECASE)]
+    }})
+    if result == []:
+        return []
+    return [(result[0]["title"], result[0]["url"])]
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result = db.search_news({"categories": {
+        "$all": [re.compile(category, re.IGNORECASE)]
+    }})
+    if result == []:
+        return []
+    return [(result[0]["title"], result[0]["url"])]
