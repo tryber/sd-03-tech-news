25e8cf02fc4a02f30add51dd4fc883cd70b39306
clayton pereira <cpereiramt@gmail.com>
2020-12-27 16:42:27 -0400

    salvando alteraçoes finais rating

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,11 +1,23 @@
 import tech_news.database as db
+from tech_news.database import client
 
 
 def top_5_news():
-    result = db.find_news()
-    if result == []:
-        return result
-    return [(result[0]["title"], result[0]["url"])]
+    # https://docs.mongodb.com/manual/reference/method/db.collection.aggregate/index.html
+    result = client.tech_news.news.aggregate([
+      {"$addFields": {
+        "total": {
+          "$add": ["$shares_count", "$comments_count"]
+        },
+      }},
+      {
+        "$limit": 5,
+      }
+    ]) or []
+    result_in_list = [
+        (item["title"], item["url"]) for item in result
+    ]
+    return result_in_list
 
 
 def top_5_categories():
