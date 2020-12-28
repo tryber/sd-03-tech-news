cc81fac41b9eabf05feb3ee028762a357fe31dad
lcdlima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-12-28 08:36:59 -0300

    Passing locally in requirement 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,28 @@
+import csv
+
+news_list = []
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError("Formato invalido")
+        with open(filepath) as file:
+            file_reader = csv.reader(file, delimiter=";")
+            header, *data = file_reader
+            for list in data:
+                for info in data:
+                    news_list.append({
+                        "url": info[0],
+                        "title": info[1],
+                        "timestamp": info[2],
+                        "writer": info[3],
+                        "shares_count": info[4],
+                        "comments_count": info[5],
+                        "summary": info[6],
+                        "sources": info[7],
+                        "categories": info[8],
+                    })
+        return news_list
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
