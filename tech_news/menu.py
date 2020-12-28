b815dba7a05af6ace565723497126b7a80f2747b
clayton pereira <cpereiramt@gmail.com>
2020-12-28 14:38:55 -0400

    salvando alteraçoes em  menu requisito bonus

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,7 @@
+import sys
+
+
+
 def collector_menu():
     print("Selecione uma das opções a seguir:\n " +
           "1 - Importar notícias a partir de um arquivo CSV;\n " +
@@ -5,7 +9,20 @@ def collector_menu():
           "3 - Raspar notícias online;\n " +
           "4 - Sair."
           )
+    user_input = input()
+    print(switch_demo(user_input))
 
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
+
+
+def switch_demo(argument):
+    # https://jaxenter.com/implement-switch-case-statement-python-138315.html
+    switcher = {
+         "1": "January",
+         "2": "February",
+         "3": "March",
+         "4": "Encerrando script\n"
+    }
+    return (switcher.get(argument, sys.stderr.write("Opção inválida\n")))
