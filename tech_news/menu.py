e4a9c370dc09232bca08aed12e24489d98dfac4b
clayton pereira <cpereiramt@gmail.com>
2020-12-28 13:39:57 -0400

    salvando alteraçoes em mostrar menu requisito bonus

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,10 @@
 def collector_menu():
-    """Seu código deve vir aqui"""
+    print("Selecione uma das opções a seguir:\n " +
+          "1 - Importar notícias a partir de um arquivo CSV;\n " +
+          "2 - Exportar notícias para CSV;\n " +
+          "3 - Raspar notícias online;\n " +
+          "4 - Sair."
+          )
 
 
 def analyzer_menu():
