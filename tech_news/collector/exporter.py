b0c1836a47f0fd787bdb130c2eb632581c522f33
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2021-01-02 15:09:19 -0300

    First commit and project done

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,35 @@
+from tech_news.database import db
+import csv
+
+
+def get_db_data():
+    return db.news.find()
+
+
+def csv_translate(data, output_file):
+    data = [doc for doc in data]
+    for doc in data:
+        del doc["_id"]
+        for field in doc:
+            print(type(doc[field]))
+            if isinstance(doc[field], list):
+                doc[field] = ",".join(doc[field])
+    header = list(data[0].keys())
+    dict_writer = csv.DictWriter(output_file, header, delimiter=";")
+    dict_writer.writeheader()
+    dict_writer.writerows(data)
+
+    return dict_writer
+
+
 def csv_exporter(filepath):
     """Seu código deve vir aqui"""
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError()
+        with open(filepath, "w") as file:
+            data = get_db_data()
+            print(data)
+            csv_translate(data, file)
+    except ValueError:
+        raise ValueError("Formato invalido")
