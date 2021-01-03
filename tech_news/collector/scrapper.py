c907ac35481d1f724a2838820eb998ffd8bc19b6
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-03 17:13:00 -0300

    requisito 1: fetch_content

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,16 @@
+import requests
+import time
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    time.sleep(delay)
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ReadTimeout):
+        return ''
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
