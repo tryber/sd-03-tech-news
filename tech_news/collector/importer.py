f9665f608902939a9a98e1fd5d5457dc15552ab0
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-03 22:23:58 -0300

    requisito 3: csv_importer

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,33 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    csv_list = []
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError("Formato invalido")
+        with open(filepath, "r") as file:
+            content = csv.reader(file, delimiter=";", quotechar='"')
+            header, *data = content
+            if (
+                "url"
+                and "title"
+                and "timestamp"
+                and "writer"
+                and "shares_count"
+                and "comments_count"
+                and "summary"
+                and "sources"
+                and "categories" not in header
+            ):
+                raise ValueError("Formato invalido")
+            for info in data:
+                new_obj = {}
+                for i in range(len(header)):
+                    key = header[i]
+                    new_obj[key] = info[i]
+                csv_list.append(new_obj)
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
+    else:
+        return csv_list
