8fcc729373650a4c0c91518c37b454684a758256
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-04 13:00:01 -0300

    requisito 9: top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,28 @@
+from tech_news.database import aggregate_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    tuples_list = []
+    data = aggregate_news(
+        [
+            {
+                "$addFields": {
+                    "popularidade": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                },
+            },
+            {
+                "$sort": {"popularidade": -1, "title": 1},
+            },
+            {
+                "$limit": 5,
+            },
+        ]
+    )
+    for new in data:
+        tuples_list.append((new["title"], new["url"]))
+    return tuples_list
 
 
 def top_5_categories():
