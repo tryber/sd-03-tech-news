45880415edd4ea084ec3e81cc720df2788abae0e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2021-01-04 15:19:23 -0300

    lint

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -12,6 +12,35 @@ from tech_news.analyzer.search_engine import (
 from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
+def importer():
+    file = input("Digite o nome do arquivo CSV a ser importado: ")
+    data = csv_importer(file)
+    create_news(data)
+
+
+def exporter():
+    file = input("Digite o nome do arquivo CSV a ser exportado: ")
+    csv_exporter(file)
+
+
+def scraper():
+    pages = input("Digite a quantidade de páginas a serem raspadas: ")
+    data = scrape(fetcher=fetch_content, pages=int(pages))
+    create_news(data)
+
+
+def exit_script():
+    print("Encerrando script\n")
+
+
+collector_options = {
+    "1": importer,
+    "2": exporter,
+    "3": scraper,
+    "4": exit_script,
+}
+
+
 def collector_menu():
     option = input(
         "Selecione uma das opções a seguir:\n "
@@ -20,21 +49,55 @@ def collector_menu():
         "3 - Raspar notícias online;\n "
         "4 - Sair."
     )
-    if option == "1":
-        file = input("Digite o nome do arquivo CSV a ser importado: ")
-        data = csv_importer(file)
-        create_news(data)
-    elif option == "2":
-        file = input("Digite o nome do arquivo CSV a ser exportado: ")
-        csv_exporter(file)
-    elif option == "3":
-        pages = input("Digite a quantidade de páginas a serem raspadas: ")
-        data = scrape(fetcher=fetch_content, pages=int(pages))
-        create_news(data)
-    elif option == "4":
-        print("Encerrando script\n")
-    else:
+    try:
+        collector_options[option]()
+    except KeyError:
         print("Opção inválida", file=sys.stderr)
+
+
+def search_title():
+    title = input("Digite o título: ")
+    search = search_by_title(title)
+    print(search)
+
+
+def search_date():
+    date = input("Digite a data no formato aaaa-mm-dd: ")
+    search = search_by_date(date)
+    print(search)
+
+
+def search_source():
+    source = input("Digite a fonte: ")
+    search = search_by_source(source)
+    print(search)
+
+
+def search_category():
+    category = input("Digite a categoria: ")
+    search = search_by_category(category)
+    print(search)
+
+
+def search_top_news():
+    news = top_5_news()
+    print(news)
+
+
+def search_top_categories():
+    categories = top_5_categories()
+    print(categories)
+
+
+analyzer_options = {
+    "1": search_title,
+    "2": search_date,
+    "3": search_source,
+    "4": search_category,
+    "5": search_top_news,
+    "6": search_top_categories,
+    "7": exit_script,
+}
 
 
 def analyzer_menu():
@@ -48,29 +111,7 @@ def analyzer_menu():
         "6 - Listar top 5 categorias;\n "
         "7 - Sair."
     )
-    if option == "1":
-        title = input("Digite o título: ")
-        search = search_by_title(title)
-        print(search)
-    elif option == "2":
-        date = input("Digite a data no formato aaaa-mm-dd: ")
-        search = search_by_date(date)
-        print(search)
-    elif option == "3":
-        source = input("Digite a fonte: ")
-        search = search_by_source(source)
-        print(search)
-    elif option == "4":
-        category = input("Digite a categoria: ")
-        search = search_by_category(category)
-        print(search)
-    elif option == "5":
-        news = top_5_news()
-        print(news)
-    elif option == "6":
-        news = top_5_categories()
-        print(news)
-    elif option == "7":
-        print("Encerrando script\n")
-    else:
+    try:
+        analyzer_options[option]()
+    except KeyError:
         print("Opção inválida", file=sys.stderr)
