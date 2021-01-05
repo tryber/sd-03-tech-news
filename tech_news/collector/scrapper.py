0021900750036991ba415ba0d83b72939bbaa78f
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2021-01-05 18:07:46 -0300

    fetch content collector scrapper

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,20 @@
+import requests
+from time import sleep
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except requests.ReadTimeout:
+        sleep(delay)
+        return ""
+    except requests.HTTPError:
+        sleep(delay)
+        return ""
+    else:
+        sleep(delay)
+        return response.text
 
 
 def scrape(fetcher, pages=1):
