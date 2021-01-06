bfe09496781d82d0ce0fda3bcbf2a8069748c58a
Mateus Talles <mateustalles@gmail.com>
2021-01-06 00:13:15 -0300

    linting test

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -18,6 +18,7 @@ def extract_quantity(arg):
     else: 
         return False
 
+
 def fetch_content(url, timeout=3, delay=0.5):
     response = requests.get(url, timeout=timeout)
     status_code = response.status_code
@@ -33,27 +34,29 @@ def scrape(fetcher, pages=1):
     news_items = fetcher("https://www.tecmundo.com.br/novidades")
     news_list = []
 
-    # for news in news_items.css(".tec--list__item"):
-    #     url = news.css(".tec--card__thumb a::attr(href)").get()
-    #     inner_selector = Selector(requests.get(url).text)
-    #     categories = inner_selector.css('[class="tec--badge tec--badge--primary"]::text').getall()
-    #     print(categories)
-    
-
 
     for news in news_items.css(".tec--list__item"):
         url = news.css(".tec--card__thumb a::attr(href)").get()
         inner_selector = Selector(requests.get(url).text)
         news_list.append({
-            "url": url,
-            "title": news.css(".tec--card__title *::text").get(),
-            "timestamp": news.css(".tec--timestamp__item.z--font-semibold *::text").get().replace('/', '-'),
-            "writer": inner_selector.css('.tec--author__info__link *::text').get(),
-            "shares_count": extract_quantity(inner_selector.css('.z--container .tec--toolbar__item *::text').re('\d')),
-            "comments_count": extract_quantity(inner_selector.css('#js-comments-btn *::text').re('\d')),
-            "summary": ''.join(inner_selector.css('.tec--article__body p:first-child *::text').getall()),
-            "sources": inner_selector.css('[class="tec--badge"]::text').getall(),
-            "categories": inner_selector.css('[class="tec--badge tec--badge--primary"]::text').getall()
+            "url": 
+                url,
+            "title":
+                news.css(".tec--card__title *::text").get(),
+            "timestamp":
+                news.css(".tec--timestamp__item.z--font-semibold *::text").get().replace('/', '-'),
+            "writer":
+                inner_selector.css('.tec--author__info__link *::text').get(),
+            "shares_count":
+                extract_quantity(inner_selector.css('.z--container .tec--toolbar__item *::text').re('\d')),
+            "comments_count":
+                extract_quantity(inner_selector.css('#js-comments-btn *::text').re('\d')),
+            "summary":
+                ''.join(inner_selector.css('.tec--article__body p:first-child *::text').getall()),
+            "sources":
+                inner_selector.css('[class="tec--badge"]::text').getall(),
+            "categories":
+                inner_selector.css('[class="tec--badge tec--badge--primary"]::text').getall()
         })
 
     print(news_list)
