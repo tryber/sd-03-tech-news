3931888751430c257d90724d4670301cead99870
Mateus Talles <mateustalles@gmail.com>
2021-01-05 23:17:17 -0300

    test changes

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,17 +1,27 @@
-import ratelimit
+# import ratelimit
 
-from ratelimit import limits
+# from ratelimit import limits
 
 import time
 import requests
-import parsel
+# import parsel
 
 from parsel import Selector
+
+
+def extract_quantity(arg):
+    if(len(arg) > 0):
+        if(isinstance(arg, list)):
+            return int(arg[0])
+        else:
+            return int(arg)
+    else: 
+        return False
 
 def fetch_content(url, timeout=3, delay=0.5):
     response = requests.get(url, timeout=timeout)
     status_code = response.status_code
-    print(status_code)
+    print(f'status code {status_code}')
     if(status_code != 200):
         return ''
     selector = Selector(text=response.text)
@@ -20,16 +30,10 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    news_items = fetcher("https://www.tecmundo.com.br/novidades").css(".tec--list__item")
+    news_items = fetcher("https://www.tecmundo.com.br/novidades")
     news_list = []
-    
-    def extract_quantity(arg):
-        if(isinstance(arg, list) and len(arg) > 0):
-            return int(arg[0])
-        else:
-            return int(arg)
 
-    for news in news_items:
+    for news in news_items.css(".tec--list__item"):
         url = news.css(".tec--card__thumb a::attr(href)").get()
         inner_selector = Selector(requests.get(url).text)
         news_list.append({
@@ -39,8 +43,9 @@ def scrape(fetcher, pages=1):
             "writer": inner_selector.css('.tec--author__info__link *::text').get(),
             "shares_count": extract_quantity(inner_selector.css('.z--container .tec--toolbar__item *::text').re('\d')),
             "comments_count": extract_quantity(inner_selector.css('#js-comments-btn *::text').re('\d')),
-            "summary": inner_selector.css('.tec--article__body p em,a:first-child::text').getall()[0]
+            # "summary": inner_selector.css('.tec--article__body').getall()
         })
-    return news_list
 
-print(scrape(fetch_content))
+    print(news_list)
+
+scrape(fetch_content)
