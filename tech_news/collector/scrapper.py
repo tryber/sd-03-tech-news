c6b475d38e87648489707bc9a8eb40020d094da4
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2021-01-07 17:19:56 -0300

    scrapper

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from time import sleep
+from parsel import Selector
 
 
 def fetch_content(url, timeout=3, delay=0.5):
@@ -18,4 +19,32 @@ def fetch_content(url, timeout=3, delay=0.5):
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    page = 1
+    all_news = []
+    while page <= pages:
+        response = fetcher("https://www.tecmundo.com.br/novidades?page={page}")
+        selector = Selector(text=response)
+        for news in selector.css(
+                ".tec--list__item .tec--card__info a::attr(href)").getall():
+            news_selector = Selector(text=fetcher(news))
+            # clean_data = counts_cleaner(
+            #     news_selector.css(".tec--toolbar__item ::text").getall())
+            all_news.append({
+                "url": news,
+                "title": news_selector
+                    .css(".tec--article__header__title::text").get(),
+                "timestamp": news_selector
+                    .css(".tec--timestamp__item time::attr(datetime)").get(),
+                "writer": news_selector
+                    .css(".tec--author__info__link::text").get(),
+                "shares_count": 0,
+                "comments_count": 0,
+                "summary": news_selector.css(
+                    ".tec--article__body *::text").get(),
+                "sources": news_selector.css(
+                    ".z--mb-16 .tec--badge::text").getall(),
+                "categories": news_selector.css(
+                    "#js-categories .tec--badge::text").getall()
+            })
+        page += 1
+    return all_news
