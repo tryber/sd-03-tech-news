7ad5e88770f799ed3ac909b2e1b429882b0c3fdf
luldelrey <rpgstrikered@gmail.com>
2021-01-06 23:16:45 -0300

    menu refactored

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -3,11 +3,37 @@ from tech_news.database import create_news
 from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import fetch_content, scrape
-from tech_news.analyzer.search_engine import search_by_title
-from tech_news.analyzer.search_engine import search_by_date
-from tech_news.analyzer.search_engine import search_by_source
-from tech_news.analyzer.search_engine import search_by_category
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    search_by_category,
+)
 from tech_news.analyzer.ratings import top_5_categories, top_5_news
+
+
+def import_csv():
+    path = input("Digite o nome do arquivo CSV a ser importado:")
+    news = csv_importer(path)
+    create_news(news)
+    return news
+
+
+def export_csv():
+    path = input("Digite o nome do arquivo CSV a ser exportado:")
+    return csv_exporter(path)
+
+
+def scrape_news():
+    pages = input("Digite a quantidade de páginas a serem raspadas:")
+    news = scrape(fetcher=fetch_content, pages=int(pages))
+    create_news(news)
+    return news
+
+
+def exit_menu():
+    sys.stdout.write("Encerrando script\n")
+    return
 
 
 def collector_menu():
@@ -20,59 +46,63 @@ def collector_menu():
  4 - Sair.
 """
     )
-    if choice == "1":
-        path = input("Digite o nome do arquivo CSV a ser importado:")
-        news = csv_importer(path)
-        create_news(news)
-        return news
-    elif choice == "2":
-        path = input("Digite o nome do arquivo CSV a ser exportado:")
-        return csv_exporter(path)
-    elif choice == "3":
-        pages = input("Digite a quantidade de páginas a serem raspadas:")
-        news = scrape(fetcher=fetch_content, pages=int(pages))
-        create_news(news)
-        return news
-    elif choice == "4":
-        sys.stdout.write("Encerrando script\n")
-        return
-    else:
+    choices = {
+        "1": import_csv,
+        "2": export_csv,
+        "3": scrape_news,
+        "4": exit_menu,
+    }
+    try:
+        choices[choice]()
+    except KeyError:
         sys.stderr.write("Opção inválida\n")
-        return
+
+
+def search_title():
+    title = input("Digite o título:")
+    return search_by_title(title)
+
+
+def search_date():
+    date = input("Digite a data no formato aaaa-mm-dd:")
+    return search_by_date(date)
+
+
+def search_source():
+    source = input("Digite a fonte:")
+    return search_by_source(source)
+
+
+def search_category():
+    category = input("Digite a categoria:")
+    return search_by_category(category)
 
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
-    choice = input("""Selecione uma das opções a seguir:
+    choice = input(
+        """Selecione uma das opções a seguir:
  1 - Buscar notícias por título;
  2 - Buscar notícias por data;
  3 - Buscar notícias por fonte;
  4 - Buscar notícias por categoria;
  5 - Listar top 5 notícias;
  6 - Listar top 5 categorias;
- 7 - Sair.""")
-    if choice == "1":
-        title = input("Digite o título:")
-        return search_by_title(title)
-    elif choice == "2":
-        date = input("Digite a data no formato aaaa-mm-dd:")
-        return search_by_date(date)
-    elif choice == "3":
-        source = input("Digite a fonte:")
-        return search_by_source(source)
-    elif choice == "4":
-        category = input("Digite a categoria:")
-        return search_by_category(category)
-    elif choice == "5":
-        return top_5_news()
-    elif choice == "6":
-        return top_5_categories()
-    elif choice == "7":
-        sys.stdout.write("Encerrando script\n")
-        return
-    else:
+ 7 - Sair."""
+    )
+    choices = {
+        "1": search_title,
+        "2": search_date,
+        "3": search_source,
+        "4": search_category,
+        "5": top_5_news,
+        "6": top_5_categories,
+        "7": exit_menu,
+    }
+    try:
+        return choices[choice]()
+    except KeyError:
         sys.stderr.write("Opção inválida\n")
-        return
 
 
 if __name__ == "__main__":
