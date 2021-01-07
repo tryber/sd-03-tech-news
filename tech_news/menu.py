cc2785440e0b9b8bccac58a368c3c7d78c286fc4
luldelrey <rpgstrikered@gmail.com>
2021-01-06 22:58:41 -0300

    project completed

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -3,6 +3,11 @@ from tech_news.database import create_news
 from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import fetch_content, scrape
+from tech_news.analyzer.search_engine import search_by_title
+from tech_news.analyzer.search_engine import search_by_date
+from tech_news.analyzer.search_engine import search_by_source
+from tech_news.analyzer.search_engine import search_by_category
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
 
 
 def collector_menu():
@@ -18,14 +23,16 @@ def collector_menu():
     if choice == "1":
         path = input("Digite o nome do arquivo CSV a ser importado:")
         news = csv_importer(path)
-        return create_news(news)
+        create_news(news)
+        return news
     elif choice == "2":
         path = input("Digite o nome do arquivo CSV a ser exportado:")
         return csv_exporter(path)
     elif choice == "3":
         pages = input("Digite a quantidade de páginas a serem raspadas:")
         news = scrape(fetcher=fetch_content, pages=int(pages))
-        return create_news(news)
+        create_news(news)
+        return news
     elif choice == "4":
         sys.stdout.write("Encerrando script\n")
         return
@@ -36,7 +43,38 @@ def collector_menu():
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    choice = input("""Selecione uma das opções a seguir:
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por fonte;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair.""")
+    if choice == "1":
+        title = input("Digite o título:")
+        return search_by_title(title)
+    elif choice == "2":
+        date = input("Digite a data no formato aaaa-mm-dd:")
+        return search_by_date(date)
+    elif choice == "3":
+        source = input("Digite a fonte:")
+        return search_by_source(source)
+    elif choice == "4":
+        category = input("Digite a categoria:")
+        return search_by_category(category)
+    elif choice == "5":
+        return top_5_news()
+    elif choice == "6":
+        return top_5_categories()
+    elif choice == "7":
+        sys.stdout.write("Encerrando script\n")
+        return
+    else:
+        sys.stderr.write("Opção inválida\n")
+        return
 
 
 if __name__ == "__main__":
     collector_menu()
+    analyzer_menu()
