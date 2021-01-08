5437aefc886936ecc0bff1a49a9b39be69931fca
Hfreitas <hfreitas86@gmail.com>
2021-01-07 21:41:48 -0300

    correção requisito 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -22,24 +22,20 @@ def get_info(fetcher, url):
     data = {}
 
     data["url"] = url
-    data["text"] = selector.css("tec--article__header__title::text").get()
-    data["timestamp"] = selector.css(
-        "tec--timestamp__item time::attr(datetime"
-    ).get()
-    data["writer"] = selector.css("tec--author__info__link::text").get()
+    data["title"] = selector.css(".tec--article__header__title::text").get()
+    data["timestamp"] = selector.css("time::attr(datetime)").get()
+    data["writer"] = (selector.css(".tec--author__info__link::text").get(),)
     shares_count = (
         selector.css("tec--toolbar__item::text").re_first(r"\d+") or "0"
     )
     data["shares-count"] = int(shares_count)
-    comments_count = (
-        selector.css("tec--toolbar__item button::text").re_first(r"\d+") or "0"
-    )
+    comments_count = selector.css("button::attr(data-count)").get() or "0"
     data["comments_count"] = int(comments_count)
-    data["summary"] = selector.css(
-        ".tec--article__body p:first-child *::text"
-    ).get()
-    data["sources"] = selector.css("a.tec--badge").getall()
-    data["categories"] = selector.css("a.tec--badge--primary").getall()
+    data["summary"] = selector.css(".tec--article__body > p::text").get()
+    data["sources"] = selector.css(".z--mb-16 div a::text").getall()
+    data["categories"] = selector.css(
+        ".tec--badge.tec--badge--primary::text"
+    ).getall()
 
     return data
 
@@ -59,3 +55,7 @@ def scrape(fetcher, pages=1):
             news.append(new)
 
     return news
+
+
+if __name__ == "__main__":
+    print(scrape(fetch_content, pages=2))
