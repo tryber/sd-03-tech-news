64e2ef367009d9838096176dbab842a3c4d66aa0
Hfreitas <hfreitas86@gmail.com>
2021-01-07 21:49:26 -0300

    requisito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,16 @@
+import csv
+from os import path
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    exists = path.exists(filepath)
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    if not exists:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
+
+    data = []
+    with open(filepath) as file:
+        csv_file = csv.DictReader(file, delimiter=";", quotechar='"')
+        data = [item for item in csv_file]
+    return data
