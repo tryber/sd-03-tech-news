2b6a007b1da203b7c77bd925a6a4ba73b7d2f4f5
Hfreitas <hfreitas86@gmail.com>
2021-01-07 21:56:16 -0300

    refatorando quesito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,16 +1,14 @@
 import csv
-from os import path
 
 
 def csv_importer(filepath):
-    exists = path.exists(filepath)
-    if not filepath.endswith(".csv"):
-        raise ValueError("Formato invalido")
-    if not exists:
-        raise ValueError("Arquivo file_not_exist.csv não encontrado")
 
-    data = []
-    with open(filepath) as file:
-        csv_file = csv.DictReader(file, delimiter=";", quotechar='"')
-        data = [item for item in csv_file]
-    return data
+    try:
+        if not filepath.endswith(".csv"):
+            raise ValueError("Formato invalido")
+        with open(filepath) as file:
+            csv_file = csv.DictReader(file, delimiter=";", quotechar='"')
+            data = [item for item in csv_file]
+            return data
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
