35c7c9ca9c07df13023ff49322d1059a6f6dc24a
wendersonbelko <contato@wendersonbelko.com>
2021-01-10 10:50:16 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,5 +1,19 @@
+import requests
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        status_code = response.status_code
+        if(status_code != 200):
+            return ''
+        selector = Selector(text=response.text)
+        request_result = selector.get()
+
+        sleep(delay)
+
+        return request_result
+    except requests.exceptions.Timeout:
+        return ''
 
 
 def scrape(fetcher, pages=1):
