d7cb51a23eb15968402ec5ecbd77bc06d2661116
Hfreitas <hfreitas86@gmail.com>
2021-01-11 18:45:43 -0300

    função de top 5 news criada

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,24 @@
+from tech_news.database import aggregate_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_news = aggregate_news(
+        [
+            {
+                "$addFields": {
+                    "popularity": {
+                        "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+            },
+            {"$sort": {"popularity": -1, "title": 1}},
+            {"$limit": 5},
+        ]
+    )
+
+    data_list = [(data["title"], data["url"]) for data in top_news]
+
+    return data_list
 
 
 def top_5_categories():
