777957f1d0c5364382b19f3226dc60d5783b9695
Hfreitas <hfreitas86@gmail.com>
2021-01-11 19:15:49 -0300

    menu collector

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -9,10 +9,50 @@ from tech_news.analyzer.search_engine import (
     search_by_source,
     search_by_category,
 )
+from tech_news.database import create_news
+
+
+def option_1():
+    file = input("Digite o nome do arquivo CSV a ser importado: ")
+    imported = csv_importer(file)
+    return create_news(imported)
+
+
+def option_2():
+    file = input("Digite o nome do arquivo CSV a ser exportado: ")
+    return csv_exporter(file)
+
+
+def option_3():
+    pages = input("Digite a quantidade de páginas a serem raspadas: ")
+    scraped = scrape(fetcher=fetch_content, pages=int(pages))
+    return create_news(scraped)
+
+
+def option_4():
+    return print("Encerrando script")
 
 
 def collector_menu():
-    """Seu código deve vir aqui"""
+    generate_menu = input(
+        "Selecione uma das opções a seguir:\n "
+        + "1 - Importar notícias a partir de um arquivo CSV;\n "
+        + "2 - Exportar notícias para CSV;\n "
+        + "3 - Raspar notícias online;\n "
+        + "4 - Sair.\n "
+    )
+
+    menu_options = {
+        "1": option_1,
+        "2": option_2,
+        "3": option_3,
+        "4": option_4,
+    }
+
+    try:
+        return menu_options[generate_menu]()
+    except KeyError:
+        return print("Opção inválida", file=sys.stderr)
 
 
 def analyzer_menu():
