f774071694c64051909e0707dafac5270dfaddfe
Hfreitas <hfreitas86@gmail.com>
2021-01-11 18:09:36 -0300

    correções na função seach_by_title, implementação da função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,12 @@
 from tech_news.database import search_news
 import re
+import datetime
 
 
 def search_by_title(title):
     db_search = search_news(
-        {"title": {"$regex": re.compile(title, re.IGNORECASE)}}
+        {"title": {"$regex": re.compile(title, re.IGNORECASE)}},
+        {"title": True, "url": True, "_id": False},
     )
 
     data_list = [(data["title"], data["url"]) for data in db_search]
@@ -13,7 +15,18 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        db_search = search_news(
+            {"timestamp": {"$regex": re.compile(date)}},
+            {"title": True, "url": True, "_id": False},
+        )
+        data_list = [(data["title"], data["url"]) for data in db_search]
+
+        return data_list
 
 
 def search_by_source(source):
