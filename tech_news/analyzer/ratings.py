8a159ffc379a777922d2b009d4f8fa06b8e7d691
gabryelryba <gabryel.ryba@gmail.com>
2021-01-11 16:25:29 -0300

    top five news aggregation

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,22 @@
+from tech_news.database import aggregate_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    topNews = []
+    data = aggregate_news([
+        {
+            "$addFields": {
+                "popularity": {"$add": ["$shares_count", "$comments_count"]}
+            }
+        },
+        {"$sort": {"popularity": -1, "title": 1}},
+        {"$limit": 5},
+    ])
+
+    for news in data:
+        topNews.append((news["title"], news["url"]))
+
+    return topNews
 
 
 def top_5_categories():
