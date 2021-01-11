c3661187f887dfadeb65babfb2dd6a633504d4a2
Hfreitas <hfreitas86@gmail.com>
2021-01-11 18:55:48 -0300

    iniciando menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,16 @@
+import sys
+from tech_news.collector.importer import csv_importer
+from tech_news.collector.exporter import csv_exporter
+from tech_news.collector.scrapper import scrape, fetch_content
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    search_by_category,
+)
+
+
 def collector_menu():
     """Seu código deve vir aqui"""
 
