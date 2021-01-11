a520ecdef6b25ceac3c1a591dc1cbb36f84b0298
Gabriel <dreadpool38@gmail.com>
2021-01-10 22:23:52 -0300

    wp

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,26 @@
+from tech_news.database import db
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    rates = []
+    query_result = db.news.find()
+    for result in query_result:
+        result["score"] = result["shares_count"] + result["comments_count"]
+        rates.append(result)
+    sorted_rates = sorted(rates, key=lambda k: k["score"])
+
+    top_rates = [(rate["title"], rate["url"]) for rate in sorted_rates][:5]
+    return top_rates
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    sorted_rates = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
+            {"$sort": {"_id": 1}},
+        ]
+    )
+    top_ids = [rate["_id"] for rate in sorted_rates][:5]
+
+    return top_ids
