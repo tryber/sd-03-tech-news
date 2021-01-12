24afc7091367470b9d10fd5699a73dcd460a4730
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-01-12 12:29:19 -0300

    import

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,20 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    response = []
+    try:
+        if not filepath.endswith('.csv'):
+            raise ValueError('Formato invalido')
+        with open(filepath, 'r') as file:
+            file_reader = csv.reader(file, delimiter=';')
+            header, *data = file_reader
+            for item in data:
+                new_item = {}
+                for i in range(len(header)):
+                    new_item[header[i]] = item[i]
+                response.append(new_item)
+    except FileNotFoundError:
+        raise ValueError("Arquivo file_not_exist.csv não encontrado")
+    else:
+        return response
