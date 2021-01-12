fe50bd8fbb1536fcef48e76b4677bbeb5a014aae
renandreolle@hotmail.com <renandreolle@hotmail.com>
2021-01-12 13:30:33 -0300

    ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,30 @@
+from tech_news.database import search_news_aggregate
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    topnews = []
+    news = search_news_aggregate([
+        {
+            "$addFields": {
+                "popularity": {"$add": ["$shares_count", "$comments_count"]}
+            }
+        },
+        {"$sort": {"popularity": -1, "title": 1}},
+        {"$limit": 5},
+    ])
+    for new in news:
+        topnews.append((new["title"], new["url"]))
+    return topnews
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    topcategories = []
+    categories = search_news_aggregate([
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ])
+    for news in categories:
+        topcategories.append((news["_id"]))
+    return topcategories
